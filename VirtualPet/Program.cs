86922b3741497e5b7c8668fcf5eeff06fb7ddb5a
Jason Denk <jdenk9@gmail.com>
2017-01-20 13:56:48 -0500

    third commit

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -11,28 +11,95 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             //variables
-            string input;
-
+            int input;
+            
             //instatiate
             VirtualPet wolf = new VirtualPet();
 
             //User sets some initial conditions
-            Console.WriteLine("Type the name of your wolf and press enter: ");
-            wolf.Name = Console.ReadLine();
-            Console.WriteLine("On a scale of 0 to 100 (100 is highest energy), type the current energy level of"+wolf.Name+" and press enter: ");
-            wolf.Energy = int.Parse(Console.ReadLine());
-            Console.WriteLine("On a scale of 0 to 100 , type the current prey drive of\n" + wolf.Name + " (100 is kill mode) and press enter: ");
-            wolf.PreyDrive = int.Parse(Console.ReadLine());
-            //show status
-            wolf.ShowInfo();
+            Console.WriteLine("Type the name of your wolf and press enter, to allow me to name your wolf, just press enter: ");
+            string name = Console.ReadLine();
+            if (name!="")
+            {
+                wolf.Name = name;
+            }         
+                        
+            
             //start owning pet
             do
             {
+                Start:;
+                //show status
+                wolf.ShowInfo();
                 Console.WriteLine("What do you want to do?");
-                Console.WriteLine("");
-                input = Console.ReadLine().ToLower();
+                Console.WriteLine("1) Feed " +wolf.Name);
+                Console.WriteLine("2) Water " + wolf.Name+"(gives water to "+wolf.Name+").");
+                Console.WriteLine("3) Play with " + wolf.Name);
+                Console.WriteLine("4) Sing " + wolf.Name + " a lullaby.");
+                Console.WriteLine("5) Let " + wolf.Name + " outside to mark territory.");
+                Console.WriteLine("6) Quit");
+                Console.WriteLine("7) Restart");
+                input = int.Parse(Console.ReadLine());
+                //checks for Restart
+                if (input==7)
+                {
+                    Console.Clear();
+                    Main(args);
+                    Environment.Exit(0);
+                }
+                //checks for valid input
+                if (input!=1&input!=2 & input !=3 & input !=4 & input !=5 & input !=6)
+                {
+                    Console.WriteLine("\nPlease type a valid response.\n");
+                    goto Start;
+                }
+                wolf.Tick();
+                //update values based off of action
+                switch (input)
+                {
+                    case 1:
+                        wolf.Feed();
+                        break;
+                    case 2:
+                        wolf.Water();
+                        break;
+                    case 3:
+                        wolf.Play();
+                        break;
+                    case 4:
+                        wolf.Sleep();
+                        break;
+                    case 5:
+                        wolf.Release();
+                        break;
+                }
+                                
+                if (wolf.Energy >= 100)
+                {
+                    Console.WriteLine(wolf.Name + " just tore your house apart and is laughing at you.  " + wolf.Name + " wants to play.");
+                    wolf.Energy = 100;
+                }
+                if(wolf.Hunger>=100)
+                {
+                    Console.WriteLine("\n"+wolf.Name + " got hungry and ate the closest piece of meat, which was you.  Sweet dreams Wolf chow...");
+                    wolf.Hunger = 0;
+                    goto PersonalDeath;
+                }                                
+
             }
-            while (input != "q");
+            while (input != 6);
+
+            PersonalDeath:
+            {
+                Console.WriteLine("\n" + "You were not successful owning this wolf.  It ate you.  Type Y and enter to try again, type anything else to exit.");
+                if(Console.ReadLine().ToLower()=="y")
+                {
+                    Console.Clear();
+                    Main(args);
+                    Environment.Exit(0);
+                }                
+            }
+
             
         }
     }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -11,7 +11,7 @@ namespace VirtualPet
         //fields
         private int energy;
         private int hunger;
-        private int thirst;
+        private int hydration;
         private int bowels;
         private int preyDrive;
         private string name;
@@ -29,7 +29,25 @@ namespace VirtualPet
             get { return this.energy; }
             set { this.energy = value; }
         }
-        //Update need to release bowels
+        //Updates Hunger Level
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+        //Updates Thirst Level
+        public int Thirst
+        {
+            get { return this.hydration; }
+            set { this.hydration = value; }
+        }
+        //Updates Bowels Level
+        public int Bowels
+        {
+            get { return this.bowels; }
+            set { this.bowels = value; }
+        }
+        //Update PreyDrive
         public int PreyDrive
         {
             get { return this.preyDrive; }
@@ -42,7 +60,7 @@ namespace VirtualPet
             this.name = "Loki";
             this.energy = 50;
             this.hunger = 50;
-            this.thirst = 50;
+            this.hydration = 50;
             this.preyDrive = 50;
             this.bowels = 50;
         }
@@ -51,61 +69,65 @@ namespace VirtualPet
         //Feed
         public void Feed()
         {
-            Console.WriteLine("You fed " + name + ".");
-            hunger += 15;
+            Console.WriteLine("\n******\n"+"You fed " + name + ".");
+            hunger -= 15;
             energy += 5;
+            hydration -= 5;
             bowels += 7;
             preyDrive -= 10;
         }
         //Give Water
         public void Water()
         {
-            Console.WriteLine("You watered " + name + ".");
-            thirst += 15;
+            Console.WriteLine("\n******\n" + "You watered " + name + ".");
+            hydration += 15;
+            hunger += 5;
             bowels += 7;
             preyDrive += 5;            
         }
         //Exercise 
         public void Play()
         {
-            Console.WriteLine("You exercised " + name + ".");
-            energy -= 15;
+            Console.WriteLine("\n******\n" + "You exercised " + name + ".");
+            energy -= 20;
             preyDrive -= 10;
-            hunger -= 10;
-            thirst -= 10;
+            hunger += 10;
+            hydration -= 15;
         }
         //Release Bowels
         public void Release()
         {
-            Console.WriteLine(name + "marked territory.");
+            Console.WriteLine("\n******\n" + name + "marked territory.");
             bowels -= 30;
             preyDrive += 5;            
         }
         //Sleep
         public void Sleep()
         {
-            Console.WriteLine(name + "slept.");
-            energy += 30;
+            Console.WriteLine("\n******\n" + name + "slept.");
+            energy += 20;
             preyDrive += 10;
-            hunger -= 5;
-            thirst -= 10;
+            hunger += 5;
+            hydration -= 10;
         }
         //Tick
         public void Tick()
         {
-            hunger += 3;
-            thirst += 3;
-            preyDrive += 10;
+            hunger += 5;
+            hydration -= 5;
+            energy += 3;
+            preyDrive += 5;
         }
         //Show info on console
         public void ShowInfo()
         {
-            Console.WriteLine(name + " the Wolf");
+            Console.WriteLine("\nNEW STATUS:\n");
+            Console.WriteLine("\n"+name + " the Wolf");
             Console.WriteLine("Energy: " + energy);
             Console.WriteLine("Hunger: " + hunger);
-            Console.WriteLine("Thirst: " + thirst);
+            Console.WriteLine("Hydrated: " + hydration);
             Console.WriteLine("Bowels: " + bowels);
-            Console.WriteLine("Prey Drive: " + preyDrive);
+            Console.WriteLine("Prey Drive: " + preyDrive+"\n");
         }
     }
 }
